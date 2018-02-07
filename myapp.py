9b486204893ab28546d71bfb55a797c6fd701aa6
Rob Green <rob@trustedhousesitters.com>
2018-02-07 13:07:28 +0000

    proper refactor of member growth panel

--- myapp.py
+++ myapp.py
@@ -28,34 +28,58 @@ df[['homeowner', 'housesitter', 'combined']] = df[['homeowner', 'housesitter', '
 # Set an inital state for plotting all member data
 init = df.groupby('period_end')[['homeowner', 'housesitter', 'combined']].sum().reset_index()
 
-colors = brewer['Dark2'][3]
 
 # Create the ColumnDataSource for plotting
-growth_source = ColumnDataSource(data=dict(
-    xs=[init.period_end,
-        init.period_end,
-        init.period_end],
-    ys=[init.homeowner,
-        init.housesitter,
-        init.combined],
-    colors=colors,
-    labels=['Owners', 'Sitters', 'Combined']))
+def create_growth_source(data):
+    source = dict(
+        xs=[data.period_end,
+            data.period_end,
+            data.period_end],
+        ys=[data.homeowner,
+            data.housesitter,
+            data.combined],
+        colors=brewer['Dark2'][3],
+        labels=['Owners', 'Sitters', 'Combined'])
+
+    return source
+
+growth_source = ColumnDataSource(data=create_growth_source(init))
+
 
 # Create figure for growth grapth
 growth_p = figure(title="Membership Growth", plot_height=400, plot_width=400, x_axis_type='datetime')
 growth_r = growth_p.multi_line(xs='xs', ys='ys', source=growth_source, color='colors', legend='labels')
 growth_p.legend.location = "top_left"
 
+
 # Create ratio ColumnDataSource for plotting
-ratio_source = ColumnDataSource(data=dict(
-    x=init.period_end,
-    y=init.housesitter / init.homeowner))
+def create_ratio_source(data):
+    source = dict(
+        x=data.period_end,
+        y=data.housesitter / data.homeowner)
+    return source
+
+ratio_source = ColumnDataSource(data=create_ratio_source(init))
 
 # Create figure for ratio graph
 ratio_p = figure(title="Membership Ratio", plot_height=400, plot_width=400, x_axis_type='datetime', y_range=(0,10))
 ratio_r = ratio_p.line(x='x', y='y', source=ratio_source, color="#2222aa", line_width=1)
 
-# Function to update plots
+
+# Generate text for counts div
+def generate_counts_html(source):
+
+    last_count = (source.data['ys'][0].shape[0]) - 1
+    owner_count = (source.data['ys'][0][last_count]).astype('str')
+    sitter_count = (source.data['ys'][1][last_count]).astype('str')
+    combined_count = (source.data['ys'][2][last_count]).astype('str')
+    
+    text = str("<ul><li>" + owner_count + " Owners</li>" + "<li>" + sitter_count + " Sitters </li>"  + "<li>" + combined_count + " Combined</li></ul>")
+    
+    return text
+
+
+# Callback to update plots
 def update(attr, old, new):
 
     country = sel_country.value
@@ -65,40 +89,30 @@ def update(attr, old, new):
     else:
         data = df[df.country_cat == country].groupby('period_end')[['homeowner', 'housesitter', 'combined']].sum().reset_index()
     
-    growth_source.data = dict(
-        xs=[data.period_end, data.period_end, data.period_end],
-        ys=[data.homeowner, data.housesitter, data.combined],
-        colors=colors,
-        labels=['Owners', 'Sitters', 'Combined'])
+    growth_source.data = create_growth_source(data)
+    ratio_source.data = create_ratio_source(data)
+    a_number.text = generate_counts_html(growth_source)
 
-    ratio_source.data = dict(
-        x=data.period_end,
-        y=data.housesitter / data.homeowner)
-
-    last_count = (growth_source.data['ys'][0].shape[0]) - 1
-    owner_count = (growth_source.data['ys'][0][last_count]).astype('str')
-    sitter_count = (growth_source.data['ys'][1][last_count]).astype('str')
-    combined_count = (growth_source.data['ys'][2][last_count]).astype('str')
-
-    a_number.text = "<ul><li>" + owner_count + " Owners</li>" + "<li>" + sitter_count + " Sitters </li>"  + "<li>" + combined_count + " Combined</li></ul>"
 
 # Select box for country
 country_options = ["All", "United Kingdom", "United States", "Australia", "Canada", "New Zealand", "ROW"]
 sel_country = Select(title="Country:", options=country_options, value="All")
 sel_country.on_change('value', update)
 
-last_count = (growth_source.data['ys'][0].shape[0]) - 1
-owner_count = (growth_source.data['ys'][0][last_count]).astype('str')
-sitter_count = (growth_source.data['ys'][1][last_count]).astype('str')
-combined_count = (growth_source.data['ys'][2][last_count]).astype('str')
-
-a_number = Div(text=("<ul><li>" + owner_count + " Owners</li>" + "<li>" + sitter_count + " Sitters </li>"  + "<li>" + combined_count + " Combined</li></ul>"),
-width=200, height=100)
+# Create div for totals
+a_number = Div(text=generate_counts_html(growth_source), width=200, height=100)
 
 # Set up layouts and add to document
-inputs = widgetbox(sel_country, a_number)
-layout = row(inputs, growth_p, ratio_p, width=1200)
-tab1 = Panel(child=layout, title="Growth")
+growth_inputs = widgetbox(sel_country, a_number)
+growth_layout = row(growth_inputs, growth_p, ratio_p, width=1200)
+
+tab1 = Panel(child=growth_layout, title="Growth")
+
+# additional tabs tab2, tab3....
+
+# List of tabs
 tabs = [tab1]
+
+# Add tabs to curdoc
 curdoc().add_root(Tabs(tabs=tabs))
 curdoc().title = "Membership Growth & Ratio"
